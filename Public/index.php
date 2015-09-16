775afa5ab09dc3d697e9d1d2bba2827669316f57
Valentin DEVILLE <contact@valentin-deville.eu>
2015-09-16 16:28:23 +0200

    Add header.php in index.php

--- Public/index.php
+++ Public/index.php
@@ -1,8 +1,9 @@
 <?php
 require '../vendor/autoload.php';
-require_once '../Application/Layouts/header.php';
+require_once '../Application/Layouts/head.php';
 
 $app = new \Slim\Slim();
+require_once '../Application/Layouts/head.php';
 $app->get('/', function ()
 {
     require_once '../Application/Modules/Home.php';
