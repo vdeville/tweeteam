2d1a7ff77d914dd14a34c0be5868543deb4c9c26
Mel Florance <mel.florance@gmail.com>
2015-09-16 20:15:28 +0200

    Parse tweet template and add tweet to list

--- Public/Js/Main.js
+++ Public/Js/Main.js
@@ -1,4 +1,11 @@
 new Twitter('192.168.1.39:3000', function(tweet)
 {
     console.log(tweet);
+    $.get('Templates/tweet.html', function(data)
+    {
+        $('#tweet_list').prepend(Mustache.render(data, tweet));
+    });
 });
+
+
+$('.tweet_date').timeago();
