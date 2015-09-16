09b5466c2c9988586243e33cbc94bdfddc2a94ac
Mel Florance <mel.florance@gmail.com>
2015-09-16 15:50:52 +0200

    Add footer and header layout

---
+++ Application/Layouts/header.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+        <script src="https://cdn.socket.io/socket.io-1.3.5.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    </head>
+</html>
