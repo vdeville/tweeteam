bce59c3cc86d71e89e9c7803dd1ad64e2fba3f9c
Mel Florance <mel.florance@gmail.com>
2015-09-16 16:51:18 +0200

    Clear file

--- Public/index.php
+++ Public/index.php
@@ -3,10 +3,12 @@ require '../vendor/autoload.php';
 require_once '../Application/Layouts/header.php';
 
 $app = new \Slim\Slim();
+
 $app->get('/', function ()
 {
     require_once '../Application/Modules/Home.php';
 });
+
 $app->run();
 
 require_once '../Application/Layouts/footer.php';
