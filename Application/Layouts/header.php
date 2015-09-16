682f9ff7571d2a109c1c733d8e1d9dd9e0dda373
Mel Florance <mel.florance@gmail.com>
2015-09-16 20:15:02 +0200

    Add Mustache and timeago to header file

--- Application/Layouts/header.php
+++ Application/Layouts/header.php
@@ -1,9 +1,11 @@
 <html>
     <head>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-timeago/1.4.2/jquery.timeago.min.js"></script>
         <script src="https://cdn.socket.io/socket.io-1.3.5.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.1.3/mustache.min.js"></script>
 
         <script src="Js/Twitter.js"></script>
         <script src="Js/Main.js"></script>
