d1d570bddbcb696df194e63393eb77bfa4997e64
Mel Florance <mel.florance@gmail.com>
2015-09-17 03:51:39 +0200

    Implement Map Class Add tweet flow control Begin scroll interraction and cache. Adding animation and transition at the onset of a tweet Add Animation marker for longitute & latitute position (shockwave!) Add Parse urls method Add retweet tooltip

--- Public/Js/Main.js
+++ Public/Js/Main.js
@@ -1,78 +1,124 @@
-var waitingTweets = [];
-var newTweets     = 0;
-var tweets        = [];
-var current       = 0;
-var flow_rate     = 2500;
-var limit         = 3;
-
-$.get('Templates/tweet.html', function(template)
+$(function($)
 {
-    new Twitter('localhost:3000', function(tweet)
+    $("#map_svg").css("width", window.innerWidth).css("height", window.innerHeight);
+
+    Map.init(function(svg, projection)
     {
-        var scroll = $(window).scrollTop();
-        if(scroll > 0)
+        var waitingTweets = [];
+        var newTweets     = 0;
+        var tweets        = [];
+        var current       = 0;
+        var flow_rate     = 2800;
+        var limit         = 50;
+
+        $.get('Templates/tweet.html', function(template)
         {
-            newTweets++;
-            waitingTweets.push(tweet);
-            $.get('Templates/waitingTweets.html', function(waiting_tpl)
+            new Twitter('localhost:3000', function(tweet)
             {
-                var sentence = 'Voir '+newTweets+' nouveau'+(newTweets>1?'x':'')+' tweet'+(newTweets>1?'s':'');
-                if($('#waitingTweets').length > 0)
+                var scroll = $(window).scrollTop();
+                if(scroll > 0)
                 {
-                    $('#waitingTweets').html(Mustache.render(waiting_tpl,
+                    newTweets++;
+                    waitingTweets.push(tweet);
+                    $.get('Templates/waitingTweets.html', function(waiting_tpl)
                     {
-                        waitingTweets: sentence
-                    }));
+                        var sentence = 'Voir '+newTweets+' nouveau'+(newTweets>1?'x':'')+' tweet'+(newTweets>1?'s':'');
+                        if($('#waitingTweets').length > 0)
+                        {
+                            $('#waitingTweets').html(Mustache.render(waiting_tpl,
+                            {
+                                waitingTweets: sentence
+                            }));
+                        }
+                        else
+                        {
+                            $('#tweet_list').prepend(Mustache.render(waiting_tpl,
+                            {
+                                waitingTweets: sentence
+                            }));
+                        }
+                    });
+                }
+                else if(waitingTweets.length == 0)
+                {
+                    var randomAnimations = ['bounceInDown', 'bounceInLeft', 'bounceInRight'];
+                    tweet.animation = randomAnimations[~~(Math.random() * randomAnimations.length)];
+                    tweet.text = parseUrls(tweet.text);
+                    if(tweet.place)
+                    {
+
+
+                        var coord =
+                        {
+                            longitude: tweet.place.bounding_box.coordinates[0][0][0],
+                            latitude : tweet.place.bounding_box.coordinates[0][0][1]
+                        };
+
+                        svg.append('circle', '.pin')
+                            .data([coord])
+                            .style('fill', '#ff0000')
+                            .attr('transform', function(d)
+                            {
+                                return 'translate('+projection(
+                                    [
+                                        d.longitude,
+                                        d.latitude
+                                    ])+')'
+                            })
+                            //.style('opacity', 0)
+                            .attr("r", 5)
+                            .transition()
+                            .duration(2000)
+                            .attr('stroke-width', 0.5)
+                            .attr("r", 25)
+                            .duration(1000)
+                            .style('opacity', 0)
+                            .transition()
+                            .duration(0)
+                            .style('opacity', 1)
+                            .attr("r", 2);
+                    }
+
+                    tweets.push(Mustache.render(template, tweet));
                 }
                 else
                 {
-                    $('#tweet_list').prepend(Mustache.render(waiting_tpl,
+                    $('#showWaitingTweets').on('click', function(event)
                     {
-                        waitingTweets: sentence
-                    }));
+                        event.preventDefault();
+                        var waiting = '';
+                        for(var t in waitingTweets)
+                            waiting += Mustache.render(template, waitingTweets[t]);
+                        $('#waitingTweets').html(waiting);
+                    });
                 }
             });
-        }
-        else if(waitingTweets.length == 0)
+        });
+
+
+        function parseUrls(text)
         {
-            var randomAnimations = ['bounceInDown', 'bounceInLeft', 'bounceInRight'];
-            tweet.animation = randomAnimations[~~(Math.random() * randomAnimations.length)];
-            tweet.text = parseUrls(tweet.text);
-            tweets.push(Mustache.render(template, tweet));
+            if(text.length && typeof text !== 'undefined')
+                return text.replace(/(https?:\/\/[^\s]+)/g, function(url)
+                { return '<a href="' + url + '" target="blank">' + url + '</a>'; });
         }
-        else
+
+        setInterval(function()
         {
-            $('#showWaitingTweets').on('click', function(event)
+            if(tweets.length > 0)
             {
-                event.preventDefault();
-                var waiting = '';
-                for(var t in waitingTweets)
-                    waiting += Mustache.render(template, waitingTweets[t]);
-                $('#waitingTweets').html(waiting);
-            });
-        }
+
+                $('#tweet_list').prepend(tweets[current]);
+                $(".tweet:not(:first)").animate({'margin-top': $('.tweet').first().height()/2});
+                $('.tweet').first().animate({'opacity': 1});
+                $('.tweet').css('margin', 0);
+                $('#tweet_list .tweet').first().addClass('tweet_border_bottom');
+                var count = $('#tweet_list .tweet').length;
+                if (count > limit) $('#tweet_list .tweet:last-child').remove();
+                current++;
+                tweets.shift();
+                $('[data-toggle="tooltip"]').tooltip();
+            }
+        }, flow_rate);
     });
 });
-
-
-function parseUrls(text)
-{
-    if(text.length && typeof text !== 'undefined')
-        return text.replace(/(https?:\/\/[^\s]+)/g, function(url)
-        { return '<a href="' + url + '" target="blank">' + url + '</a>'; });
-}
-
-setInterval(function()
-{
-    if(tweets.length > 0)
-    {
-        $('#tweet_list').prepend(tweets[current]);
-        $('#tweet_list .tweet').first().addClass('tweet_border_bottom');
-        var count = $('#tweet_list .tweet').length;
-        if (count > limit) $('#tweet_list .tweet:last-child').remove();
-        current++;
-        tweets.shift();
-        console.log(tweets.length);
-        $('[data-toggle="tooltip"]').tooltip();
-    }
-}, flow_rate);
