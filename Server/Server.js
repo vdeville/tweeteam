bf217bfebbbfabd2661b085f1d5854b1c0c9bb16
Mel Florance <mel.florance@gmail.com>
2015-09-17 11:10:16 +0200

    Change package system for tweet getting

--- Server/Server.js
+++ Server/Server.js
@@ -1,8 +1,8 @@
 var fs = require("fs"),
     app = require("http").createServer(handler),
     io = require("socket.io").listen(app, { log: false }),
-    theport = process.env.PORT || 3000,
-    twitter = require("ntwitter");
+    theport = process.env.PORT || 3000;
+
 
 app.listen(theport);
 console.log ("http server on port: " + theport);
@@ -11,26 +11,21 @@ function handler (req, res) {
 
 }
 
-var tw = new twitter({
+
+var Stream = require('user-stream');
+var twitter = new Stream({
     consumer_key        : "O4Hhtf1mTUfCMmICOGNcLPP9X",
     consumer_secret     : "c8todcqVU2w5Fk51Qk0aLukvyIwGJ1V4x4NLpTKGq9TEyLWc1f",
     access_token_key    : "2478411144-b08QFN42lFhe8INmBIpmob74OefFGS85uAeI29j",
     access_token_secret : "SxSCqKIjMRoJKsbH4lSd1ikETe8I9PO8kpl2GiGnifw0Y"
-}), users = [], nbTweets = 0;
-
-var creds = tw.verifyCredentials(function(err, response) {
 });
 
-console.log(creds);
-
-
-var track = 'a,e,i,o,u';
+var users = [], nbTweets = 0;
 
 io.sockets.on("connection", function(socket)
 {
     if (users.indexOf(socket.id) === -1)
         users.push(socket.id);
-
 
     logConnectedUsers();
 
@@ -42,39 +37,25 @@ io.sockets.on("connection", function(socket)
     });
 });
 
-tw.stream("statuses/filter",
-{
-    track: track
-},
-function(stream)
-{
 
-    stream.on("data", function(data)
-    {
-        nbTweets++;
-        io.sockets.emit('new tweet',
-        {
-            id: data.id_str,
-            text: data.text,
-            created_at: data.created_at,
-            screen_name: data.user.screen_name,
-            place: data.place
-        });
-    });
-
-    stream.on('error', function(error)
-    {
-        console.log(error);
-    });
+twitter.stream(
+{
+    with: 'fhollande, elysee, vir4x, elysee_com'
 });
 
-function resolveLocation(location)
+twitter.on('data', function(data)
 {
-    tw.get('https://api.twitter.com/1.1/geo/id/df51dec6f4ee2b2c.json', {'place_id': location}, function(data)
+    console.log(data);
+    nbTweets++;
+    io.sockets.emit('new tweet',
     {
-        console.log(data);
+        id: data.id_str,
+        text: data.text,
+        created_at: data.created_at,
+        screen_name: data.user.screen_name,
+        place: data.place
     });
-}
+});
 
 function logConnectedUsers() {
     console.log("============= CONNECTED USERS ==============");
